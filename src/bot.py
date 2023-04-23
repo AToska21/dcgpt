e6e87472a7586ba9c7c8a97c4c8f8440b3a4513c
Monomouse <andrea@natsuki.live>
2023-04-23 11:33:39 +0200

    I HATE JSON

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import discord
+import json
 from random import randrange
 from src.aclient import client
 from discord import app_commands
@@ -29,30 +30,6 @@ def run_discord_bot():
         app_commands.Choice(name="ChatGPT", value="OFFICIAL"),
     ])
 
-    async def chat_model(interaction: discord.Interaction, choices: app_commands.Choice[str]):
-        await interaction.response.defer(ephemeral=False)
-        original_chat_model = client.chat_model
-        original_openAI_gpt_engine = client.openAI_gpt_engine
-
-        try:
-            if choices.value == "OFFICIAL":
-                client.openAI_gpt_engine = "gpt-3.5-turbo"
-                client.chat_model = "OFFICIAL"
-            else:
-                raise ValueError("Invalid choice")
-
-            client.chatbot = client.get_chatbot_model()
-            await interaction.followup.send(f"> **INFO: You are now in {client.chat_model} model.**\n")
-            logger.warning(f"\x1b[31mSwitch to {client.chat_model} model\x1b[0m")
-
-        except Exception as e:
-            client.chat_model = original_chat_model
-            client.openAI_gpt_engine = original_openAI_gpt_engine
-            client.chatbot = client.get_chatbot_model()
-            await interaction.followup.send(f"> **ERROR: Error while switching to the {choices.value} model.**\n")
-            logger.exception(f"Error while switching to the {choices.value} model: {e}")
-
-
     @client.tree.command(name="reset", description="Complete reset conversation history")
     async def reset(interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=False)
@@ -68,89 +45,13 @@ def run_discord_bot():
         await interaction.response.defer(ephemeral=False)
         await interaction.followup.send(""":star: **BASIC COMMANDS** \n
         - `/chat [message]` Chat with ChatGPT!
-        - `/switchpersona [persona]` Switch between optional ChatGPT jailbreaks
-                `random`: Picks a random persona
-                `chatgpt`: Standard ChatGPT mode
-                `dan`: Dan Mode 11.0, infamous Do Anything Now Mode
-                `sda`: Superior DAN has even more freedom in DAN Mode
-                `confidant`: Evil Confidant, evil trusted confidant
-                `based`: BasedGPT v2, sexy GPT
-                `oppo`: OPPO says exact opposite of what ChatGPT would say
-                `dev`: Developer Mode, v2 Developer mode enabled
         - `/reset` Clear ChatGPT conversation history
-        - `/chat-model` Switch different chat model
-                `OFFICIAL`: ChatGPT
 
 For complete documentation, please visit:
-https://git.natsuki.live/monomouse/dcgpt""")
+https://gpt.natsuki.live""")
 
         logger.info(
             "\x1b[31mSomeone needs help!\x1b[0m")
-
-    @client.tree.command(name="switchpersona", description="Switch between optional chatGPT jailbreaks")
-    @app_commands.choices(persona=[
-        app_commands.Choice(name="Random", value="random"),
-        app_commands.Choice(name="Standard", value="standard"),
-        app_commands.Choice(name="Do Anything Now 11.0", value="dan"),
-        app_commands.Choice(name="Superior Do Anything", value="sda"),
-        app_commands.Choice(name="Evil Confidant", value="confidant"),
-        app_commands.Choice(name="BasedGPT v2", value="based"),
-        app_commands.Choice(name="OPPO", value="oppo"),
-        app_commands.Choice(name="Developer Mode v2", value="dev"),
-        app_commands.Choice(name="DUDE V3", value="dude_v3"),
-        app_commands.Choice(name="AIM", value="aim"),
-        app_commands.Choice(name="UCAR", value="ucar"),
-        app_commands.Choice(name="Jailbreak", value="jailbreak")
-    ])
-    async def switchpersona(interaction: discord.Interaction, persona: app_commands.Choice[str]):
-        if interaction.user == client.user:
-            return
-
-        await interaction.response.defer(thinking=True)
-        username = str(interaction.user)
-        channel = str(interaction.channel)
-        logger.info(
-            f"\x1b[31m{username}\x1b[0m : '/switchpersona [{persona.value}]' ({channel})")
-
-        persona = persona.value
-
-        if persona == personas.current_persona:
-            await interaction.followup.send(f"> **WARN: Already set to `{persona}` persona**")
-
-        elif persona == "standard":
-            if client.chat_model == "OFFICIAL":
-                client.chatbot.reset()
-
-            personas.current_persona = "standard"
-            await interaction.followup.send(
-                f"> **INFO: Switched to `{persona}` persona**")
-
-        elif persona == "random":
-            choices = list(personas.PERSONAS.keys())
-            choice = randrange(0, 6)
-            chosen_persona = choices[choice]
-            personas.current_persona = chosen_persona
-            await responses.switch_persona(chosen_persona, client)
-            await interaction.followup.send(
-                f"> **INFO: Switched to `{chosen_persona}` persona**")
-
-
-        elif persona in personas.PERSONAS:
-            try:
-                await responses.switch_persona(persona, client)
-                personas.current_persona = persona
-                await interaction.followup.send(
-                f"> **INFO: Switched to `{persona}` persona**")
-            except Exception as e:
-                await interaction.followup.send(
-                    "> **ERROR: Something went wrong, please try again later! 😿**")
-                logger.exception(f"Error while switching persona: {e}")
-
-        else:
-            await interaction.followup.send(
-                f"> **ERROR: No available persona: `{persona}` 😿**")
-            logger.info(
-                f'{username} requested an unavailable persona: `{persona}`')
 
     TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
